b08bc6f9a70af7a7fdc125b789ba709be9403759
matpek <matiaspekarek@gmail.com>
2025-10-15 18:00:47 -0300

    agregue favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/svg+xml" href="Favicon.svg" />
         {ENABLE_GTM && (
           <script
             dangerouslySetInnerHTML={{
